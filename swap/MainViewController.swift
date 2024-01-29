e822da95507baec9a0f45026a8a85cfcbc59bfbe
gleam0807 <teemocscsg@naver.com>
2024-01-29 22:29:39 +0900

    3.1

---
+++ swap/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  swap
+//
+//  Created by SUNG on 1/29/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
--- swap/SuggestionViewController.swift
+++ swap/SuggestionViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class SuggestionViewController: UIViewController {
+    //MARK: Outlet
+    @IBOutlet weak var SuggestionTableView: UITableView!
+    
     override func viewDidLoad() {
-        
+        SuggestionTableView.dataSource = self
+        SuggestionTableView.delegate = self
     }
     
     //MARK: Action
     @IBAction func prevButtonClicked(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
+    @IBAction func jumpButtonClicked(_ sender: UIButton) {
+        if let mainVC = storyboard?.instantiateViewController(withIdentifier: "MainViewController") {
+            mainVC.modalPresentationStyle = .fullScreen
+            present(mainVC, animated: true)
+        }
+    }
+}
+
+extension SuggestionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SuggestionTableViewCell", for: indexPath) as? SuggestionTableViewCell else { return UITableViewCell() }
+        cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+        
+        return cell
+    }
+    
+    
+}
+
+extension SuggestionViewController: UITableViewDelegate {
+    
+}
+
+class SuggestionTableViewCell: UITableViewCell {
+    
 }
